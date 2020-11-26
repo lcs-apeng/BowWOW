52bfb23dfdfec39e09ff57ea779a0da2f52a84ac
ameliapeng <apeng@lcs.on.ca>
2020-11-26 09:54:22 -0500

    finished with the BowWOW app! works perfectly

---
+++ BowWOW/RandomDog.swift
@@ -0,0 +1,13 @@
+//
+//  RamdonDog.swift
+//  BowWOW
+//
+//  Created by amelia peng on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomDog: Codable {
+    var message: String
+    var status: String
+}
